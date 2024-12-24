fbc5f592a60a0de1a6fc1142c5ce1787832a3917
sao-v <saovarun0406@outlook.com>
2024-12-24 14:44:22 +0530

    test

---
+++ src/Check_Vowel.java
@@ -0,0 +1,43 @@
+public class Check_Vowel {
+    static char check_vowel(char a){
+
+        switch (a){
+            case 'a':
+                System.out.println("Is vowel");
+                break;
+            case 'e' :
+                System.out.println("Is vowel");
+                break;
+            case 'i' :
+                System.out.println("Is vowel");
+                break;
+            case 'o' :
+                System.out.println("is vowel");
+                break;
+            case 'u' :
+                System.out.println("is vowel");
+                break;
+            case 'A':
+                System.out.println("Is vowel");
+                break;
+            case 'E' :
+                System.out.println("Is vowel");
+                break;
+            case 'I' :
+                System.out.println("Is vowel");
+                break;
+            case 'O' :
+                System.out.println("is vowel");
+                break;
+            case 'U' :
+                System.out.println("is vowel");
+                break;
+            default:
+                System.out.println("Is not a vowel");
+        }
+        return a;
+    }
+    public static void main(String[] args){
+        check_vowel('a');
+    }
+}

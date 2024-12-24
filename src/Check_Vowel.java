c361ca91d1997cf4f8dd7c61f5b035047dc9f336
sao-v <saovarun0406@outlook.com>
2024-12-24 14:45:09 +0530

    test

--- src/Check_Vowel.java
+++ src/Check_Vowel.java
@@ -35,7 +35,7 @@ public class Check_Vowel {
             default:
                 System.out.println("Is not a vowel");
         }
-        return a;
+        return 0;
     }
     public static void main(String[] args){
         check_vowel('a');
